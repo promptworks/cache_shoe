09a2d24963674ab784494a244a2648482447d97d
Justin Campbell <justin@justincampbell.me>
2015-01-23 16:44:13 -0500

    Move on_cache_clear to Cacher

--- lib/cache_shoe.rb
+++ lib/cache_shoe.rb
@@ -23,16 +23,6 @@ module CacheShoe
 
   private
 
-  def self.get_cache_args(key_extractor, *args)
-    case key_extractor
-    when PASS_THROUGH then args
-    when Proc         then [key_extractor.call(*args)].flatten
-    when Symbol       then [args.first.send(key_extractor)]
-    else
-      fail "Can't create a cache key from #{key_extractor.inspect}"
-    end
-  end
-
   def self.on_cache_hit(key_val)
     if config.on_cache
       config.on_cache.call(key_val, :hit)
@@ -47,39 +37,12 @@ module CacheShoe
     logger.info "cache miss #{key_val}"
   end
 
-  def self.set_cache(key_val, cache_val)
-    cache.write(key_val, nil_wrapper(cache_val))
-    nil_wrapper(cache_val)
-  end
-
-  def self.cache_method_clear(key)
-    cache.delete(key)
-  end
-
   def self.cache
     config.cache
   end
 
   def self.logger
     config.logger
-  end
-
-  def self.on_cache_clear(scope)
-    Array(scope.key_extractors).each do |key_extractor|
-      begin
-        cache_args = get_cache_args(key_extractor, *(scope.args))
-        cached_key = scope.cache_key(cache_args)
-        logger.info "Clearing cache from #{scope.clearing_method}: #{cached_key}"
-        if config.on_cache_clear
-          config.on_cache_clear.call(
-            cached_key, scope.clearing_method)
-        end
-        cache_method_clear cached_key
-      rescue
-        logger.error "Failed to clear cache from #{scope.class_name}." \
-          "#{scope.clearing_method}, because the key extractor raised"
-      end
-    end
   end
 
   module ClassMethods
--- lib/cache_shoe/cacher.rb
+++ lib/cache_shoe/cacher.rb
@@ -19,9 +19,47 @@ module CacheShoe
     end
 
     def invalidate
-      CacheShoe.on_cache_clear(scope)
+      on_cache_clear
 
       yield
+    end
+
+    private
+
+    def get_cache_args(key_extractor, *args)
+      case key_extractor
+      when PASS_THROUGH then args
+      when Proc         then [key_extractor.call(*args)].flatten
+      when Symbol       then [args.first.send(key_extractor)]
+      else
+        fail "Can't create a cache key from #{key_extractor.inspect}"
+      end
+    end
+
+    def on_cache_clear
+      Array(scope.key_extractors).each do |key_extractor|
+        begin
+          cache_args = get_cache_args(key_extractor, *(scope.args))
+          cached_key = scope.cache_key(cache_args)
+          logger.info "Clearing cache from #{scope.clearing_method}: #{cached_key}"
+          if config.on_cache_clear
+            config.on_cache_clear.call(
+              cached_key, scope.clearing_method)
+          end
+          config.cache.delete cached_key
+        rescue
+          logger.error "Failed to clear cache from #{scope.class_name}." \
+            "#{scope.clearing_method}, because the key extractor raised"
+        end
+      end
+    end
+
+    def config
+      CacheShoe.config
+    end
+
+    def logger
+      CacheShoe.logger
     end
   end
 end
