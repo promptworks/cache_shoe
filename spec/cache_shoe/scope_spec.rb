25ac66a38475669c02608cf36afbababf65261e4
Justin Campbell <justin@justincampbell.me>
2015-01-26 16:59:59 -0500

    Add specs to Scope

--- spec/cache_shoe/scope_spec.rb
+++ spec/cache_shoe/scope_spec.rb
@@ -1,11 +1,49 @@
 RSpec.describe CacheShoe::Scope do
   subject(:scope) { described_class.new(options) }
 
-  context "with a single key extractor" do
-    let(:options) { { key_extractors: :foo } }
+  let(:options) {
+    { object: object, cached_method: cached_method, args: args }
+  }
 
-    it "wraps it in an array" do
-      expect(scope.key_extractors).to match_array([:foo])
+  let(:object) { Object.new }
+  let(:cached_method) { :foo }
+  let(:args) { ["abc"] }
+
+  describe "#cache_key" do
+    subject(:cache_key) { scope.cache_key }
+
+    context "with all types of arguments" do
+      def method_with_lots_of_args(first = nil, *second, third: nil, **fourth)
+        local_variables.map { |var| instance_eval(var.to_s) }
+      end
+
+      let(:args) {
+        method_with_lots_of_args(
+          "first", 2, 3, third: true, fourth: false, fifth: :sym
+        )
+      }
+
+      it "stringifys each argument" do
+        expect(cache_key).to eq(
+          'Object::foo::["first", [2, 3], true, {:fourth=>false, :fifth=>:sym}]'
+        )
+      end
+    end
+  end
+
+  describe "#class_name" do
+    it "derives from the object" do
+      expect(scope.class_name).to eq("Object")
+    end
+  end
+
+  describe "#key_extractors" do
+    context "with a single key extractor" do
+      let(:options) { { key_extractors: :foo } }
+
+      it "wraps it in an array" do
+        expect(scope.key_extractors).to match_array([:foo])
+      end
     end
   end
 end
