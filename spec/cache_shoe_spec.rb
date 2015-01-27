0b577f18af3ba16e2bc47472d9bbf21d6e5f0e84
Jeff Deville <jeffdeville@gmail.com>
2015-01-27 11:52:49 -0500

    shut up rubocop

--- spec/cache_shoe_spec.rb
+++ spec/cache_shoe_spec.rb
@@ -1,23 +1,23 @@
-RSpec.describe "when caching a service-style object" do
+RSpec.describe 'when caching a service-style object' do
   Given { CacheShoe.config.cache.clear }
   Given(:thing) { Thing.new(123, 'name') }
 
-  shared_examples_for "caching" do
-    context "read caching" do
-      context "when nil, it should cache the nil" do
-        Given { service_ex.read("INVALID_ID") }          # cache the nil
-        When(:result) { service_ex.read("INVALID_ID") }  # now hit the cache
+  shared_examples_for 'caching' do
+    context 'read caching' do
+      context 'when nil, it should cache the nil' do
+        Given { service_ex.read('INVALID_ID') }          # cache the nil
+        When(:result) { service_ex.read('INVALID_ID') }  # now hit the cache
         Then  { service_ex.read_calls == 1 }
       end
 
-      context "when not nil, it should cache that" do
+      context 'when not nil, it should cache that' do
         Given { service_ex.create thing }
         When  { 2.times { service_ex.read(thing.id) } }
         Then  { service_ex.read_calls == 1 }
       end
     end
 
-    context "when creating it wipes the cache" do
+    context 'when creating it wipes the cache' do
       Given { service_ex.read(123) } # populate the cache with nil
       When { service_ex.create thing }
       Given(:result) { service_ex.read(123) }
@@ -25,7 +25,7 @@ RSpec.describe "when caching a service-style object" do
       And  { service_ex.read_calls == 2 }
     end
 
-    context "when deleting it wipes the cache" do
+    context 'when deleting it wipes the cache' do
       Given do
         service_ex.create thing
         # Populate the cache
@@ -42,21 +42,26 @@ RSpec.describe "when caching a service-style object" do
     end
   end
 
-  context "when using procs to clearing the cache" do
+  context 'when using procs to clearing the cache' do
     Given(:service_ex) { ProcServiceExample.new }
-    it_should_behave_like "caching"
+    it_should_behave_like 'caching'
   end
 
-  context "when using symbol keys to clear the cache" do
+  context 'when using symbol keys to clear the cache' do
     Given(:service_ex) { KeyServiceExample.new }
-    it_should_behave_like "caching"
+    it_should_behave_like 'caching'
   end
 
-  context "when caching multiple methods" do
-    Given(:service_ex) { CacheTwoMethodsExample.new }
-    it_should_behave_like "caching"
+  context 'when omitting the model class' do
+    Given(:service_ex) { DefaultModelServiceExample.new }
+    it_should_behave_like 'caching'
+  end
 
-    shared_context "create thing and cache the reads" do
+  context 'when caching multiple methods' do
+    Given(:service_ex) { CacheTwoMethodsExample.new }
+    it_should_behave_like 'caching'
+
+    shared_context 'create thing and cache the reads' do
       Given do
         service_ex.create thing
         service_ex.read 123
@@ -64,8 +69,8 @@ RSpec.describe "when caching a service-style object" do
       end
     end
 
-    context "caching works" do
-      include_context "create thing and cache the reads"
+    context 'caching works' do
+      include_context 'create thing and cache the reads'
       When(:result) do
         [service_ex.read(123), service_ex.read_hash(id: 123)]
       end
@@ -74,8 +79,8 @@ RSpec.describe "when caching a service-style object" do
       And { service_ex.read_calls == 1 }
     end
 
-    context "cache clearing works" do
-      include_context "create thing and cache the reads"
+    context 'cache clearing works' do
+      include_context 'create thing and cache the reads'
       When(:result) do
         service_ex.delete 123
         [service_ex.read(123), service_ex.read_hash(id: 123)]
@@ -87,11 +92,11 @@ RSpec.describe "when caching a service-style object" do
     end
   end
 
-  context "when clearing multiple keys" do
+  context 'when clearing multiple keys' do
     Given(:service_ex) { MultiKeyClearExample.new }
-    it_should_behave_like "caching"
+    it_should_behave_like 'caching'
 
-    context "ensure both keys are cleared" do
+    context 'ensure both keys are cleared' do
       Given do
         service_ex.read thing.id
         service_ex.read thing.name
@@ -107,13 +112,29 @@ RSpec.describe "when caching a service-style object" do
     end
   end
 
-  context "when the cache methods raise" do
+  context 'when clearing a key cached by another service' do
+    Given(:service_ex) { KeyServiceExample.new }
+    Given(:clearing_service_ex) do
+      SeparateCleaningServiceExample.new(service_ex)
+    end
+    Given do
+      service_ex.create thing
+      service_ex.read(thing.id)
+    end
+
+    When { clearing_service_ex.delete(thing.id) }
+
+    Then { service_ex.read(thing.id).nil? }
+    And  { service_ex.read_calls == 2 }
+  end
+
+  context 'when the cache methods raise' do
     Given(:service_ex) { BrokenExample.new }
     When(:result) { service_ex.create thing }
     Then { service_ex.create_calls == 1 }
   end
 
-  context "when read cache takes multiple parameters" do
+  context 'when read cache takes multiple parameters' do
     Given(:service_ex) { TwoParameterProcExample.new }
     Given { service_ex.read thing.id, thing.name } # cache
     When(:result) do
